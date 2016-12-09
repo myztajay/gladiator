87d07f7e101a1307aecbbdfa6dca0fa27f1675b6
myztajay <myztajay@hotmail.com>
2016-12-09 14:58:47 -0500

    added methods for fight and add gladiator

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,22 @@
+require'byebug'
+
 class Arena
+attr_accessor :name, :gladiators
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+  # adds gladiators to
+  def add_gladiator(new_glad)
+    if @gladiators.length < 2
+    @gladiators.push(new_glad)
+    end
+  end
+
+  def fight
+    if @gladiators.length < 2
+      # @gladiators.first.weapon ==
+    end
+
+  end
 end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,7 @@
 class Gladiator
+  attr_accessor :name, :weapon
+  def initialize(name,weapon)
+    @name = name
+    @weapon = weapon
+  end
 end
